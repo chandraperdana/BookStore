718f2fd3a46e3dda268089314fd53bc658b18c40
Chandra Perdana <46883004+chandraperdana@users.noreply.github.com>
2022-09-10 02:43:23 +0700

    Add project files.

---
+++ BookStore.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using BookStore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.DataAccess
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CoverType> CoverTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+
+
+    //public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    //{
+    //    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    //        : base(options)
+    //    {
+    //    }
+
+    //    protected override void OnModelCreating(ModelBuilder builder)
+    //    {
+    //        base.OnModelCreating(builder);
+    //        // Customize the ASP.NET Identity model and override the defaults if needed.
+    //        // For example, you can rename the ASP.NET Identity table names and more.
+    //        // Add your customizations after calling base.OnModelCreating(builder);
+    //    }
+    //}
+}
